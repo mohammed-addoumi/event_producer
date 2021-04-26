e1bfff57f57815f7c98182d0a66f27d4973cf8f1
mohammed addoumi <addoumi.mohammed@gmail.com>
2021-04-26 23:06:36 +0000

    creating database table file and data initializing file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO event(id,name,event_type,status) VALUES(1,'event1','INSERT','INITIATED');
+INSERT INTO event(id,name,event_type,status) VALUES(2,'event2','UPDATE','INITIATED');
+INSERT INTO event(id,name,event_type,status) VALUES(3,'event3','DELETE','INITIATED');
+INSERT INTO event(id,name,event_type),status) VALUES(4,'event4','UPDATE','INITIATED');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS "event";
+CREATE TABLE event (
+    id   INTEGER      NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    event_type VARCHAR(128) NOT NULL,
+    status VARCHAR(128) NOT NULL,
+    PRIMARY KEY (id)
+);
